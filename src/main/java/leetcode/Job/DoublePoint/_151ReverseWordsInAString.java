db3b94d01d6fb50a2e94c44c444b8e8200e35a6a
Huali Zhang <zhanghl@rd.netease.com>
2021-03-16 15:10:49 +0800

    双指针题目

---
+++ src/main/java/leetcode/Job/DoublePoint/_151ReverseWordsInAString.java
@@ -0,0 +1,194 @@
+package leetcode.Job.DoublePoint;
+
+//给定一个字符串，逐个翻转字符串中的每个单词。 
+//
+// 说明： 
+//
+// 
+// 无空格字符构成一个 单词 。 
+// 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。 
+// 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入："the sky is blue"
+//输出："blue is sky the"
+// 
+//
+// 示例 2： 
+//
+// 输入："  hello world!  "
+//输出："world! hello"
+//解释：输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+// 
+//
+// 示例 3： 
+//
+// 输入："a good   example"
+//输出："example good a"
+//解释：如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+// 
+//
+// 示例 4： 
+//
+// 输入：s = "  Bob    Loves  Alice   "
+//输出："Alice Loves Bob"
+// 
+//
+// 示例 5： 
+//
+// 输入：s = "Alice does not even like bob"
+//输出："bob like even not does Alice"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 104 
+// s 包含英文大小写字母、数字和空格 ' ' 
+// s 中 至少存在一个 单词 
+// 
+//
+// 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 请尝试使用 O(1) 额外空间复杂度的原地解法。 
+// 
+// Related Topics 字符串 
+// 👍 290 👎 0
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class _151ReverseWordsInAString {
+    public static void main(String[] args) {
+        Solution solution = new _151ReverseWordsInAString().new Solution();
+        solution.reverseWords("the sky is blue");
+    }
+
+    /**
+     *
+     */
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        //TODO :不要使用辅助空间，空间复杂度要求为O(1).目前没有发现更好的方式
+
+        //时间复杂度：O(N)，其中 N 为输入字符串的长度。
+        //
+        //空间复杂度：O(N)，双端队列存储单词需要 O(N) 的空间。
+        public String reverseWords2(String s) {
+            int left = 0, right = s.length() - 1;
+            // 去掉字符串开头的空白字符
+            while (left <= right && s.charAt(left) == ' ') {
+                ++left;
+            }
+
+            // 去掉字符串末尾的空白字符
+            while (left <= right && s.charAt(right) == ' ') {
+                --right;
+            }
+
+            Deque<String> d = new ArrayDeque<String>();
+            StringBuilder word = new StringBuilder();
+
+            while (left <= right) {
+                char c = s.charAt(left);
+                if ((word.length() != 0) && (c == ' ')) {
+                    // 将单词 push 到队列的头部
+                    d.offerFirst(word.toString());
+                    word.setLength(0);
+                } else if (c != ' ') {
+                    word.append(c);
+                }
+                ++left;
+            }
+            d.offerFirst(word.toString());
+
+            return String.join(" ", d);
+        }
+
+        //时间复杂度：O(N)，其中 N 为输入字符串的长度。
+        //
+        //空间复杂度：Java 和 Python 的方法需要 O(N) 的空间来存储字符串，而 C++ 方法只需要 O(1) 的额外空间来存放若干变量。
+
+        /**
+         * @Description: 算法过程：
+         * 先去除首尾以及中间多余的空格，中间单词之间只保留一个空格
+         * 然后把整个字符串进行翻转，在翻转单词
+         * @Param: [s]
+         */
+        public String reverseWords(String s) {
+            //先去除首尾以及中间多余的空格，中间单词之间只保留一个空格
+            //由   leetcode is   fun  变为leetcode is fun
+            StringBuilder sb = trimSpace(s);
+            //整个字符串进行翻转, 变为nuf si edocteel
+            reverseString(sb, 0, sb.length() - 1);
+            //翻转单词 变为：fun is leetcode
+            reverseEachWord(sb);
+            return sb.toString();
+        }
+
+        private void reverseEachWord(StringBuilder sb) {
+            int start = 0;
+            int end = 0;//表示单词的首尾
+            while (start < sb.length()) {
+                while (end < sb.length() && sb.charAt(end) != ' ') {
+                    end++;
+                }
+                reverseString(sb, start, end - 1);
+                start = end + 1;
+                end++;
+            }
+        }
+
+        private StringBuilder trimSpace(String s) {
+            int left = 0;
+            int right = s.length() - 1;
+            StringBuilder stringBuilder = new StringBuilder();
+            while (left <= right && s.charAt(left) == ' ') {
+                left++;
+            }
+            while (left <= right && s.charAt(right) == ' ') {
+                right--;
+            }
+            while (left <= right) {
+                char c = s.charAt(left);
+                if (c != ' ') {
+                    stringBuilder.append(c);
+                } else if (stringBuilder.charAt(stringBuilder.length() - 1) != ' ') {
+                    stringBuilder.append(c);
+                }
+                left++;
+            }
+            return stringBuilder;
+        }
+
+        void reverseString(StringBuilder s, int left, int right) {
+            while (left < right) {
+                swap(s, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        private void swap(StringBuilder s, int a, int b) {
+            char temp = s.charAt(a);
+            s.setCharAt(a, s.charAt(b));
+            s.setCharAt(b, temp);
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/leetcode/Job/DoublePoint/_206ReverseLinkedList.java
@@ -0,0 +1,88 @@
+package leetcode.Job.DoublePoint;
+
+//反转一个单链表。 
+//
+// 示例: 
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL 
+//
+// 进阶: 
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？ 
+// Related Topics 链表 
+// 👍 1546 👎 0
+
+
+import leetcode.ListNode;
+
+public class _206ReverseLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new _206ReverseLinkedList().new Solution();
+    }
+    /**
+
+     */
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        /**
+         * @Description: 首先定义一个cur指针，指向头结点，再定义一个pre指针，初始化为null。
+         * <p>
+         * 然后就要开始反转了，首先要把 cur->next 节点用tmp指针保存一下，也就是保存一下这个节点。
+         * <p>
+         * 为什么要保存一下这个节点呢，因为接下来要改变 cur->next 的指向了，将cur->next 指向pre ，此时已经反转了第一个节点了。
+         * <p>
+         * 接下来，就是循环走如下代码逻辑了，继续移动pre和cur指针。
+         * <p>
+         * 最后，cur 指针已经指向了null，循环结束，链表也反转完毕了。此时我们return pre指针就可以了，pre指针就指向了新的头结点。
+         * @Param: [head]
+         */
+        public ListNode reverseList1(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode pre = null;
+            ListNode cur = head;
+            while (cur != null) {
+                ListNode temp = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = temp;
+            }
+            return pre;
+        }
+
+        /**
+         * @Description: 递归
+         * @Param:
+         */
+        public ListNode reverseList(ListNode head) {
+            ListNode pre = null;
+            ListNode cur = head;
+            return reverse(pre, cur);
+        }
+
+        public ListNode reverse(ListNode pre, ListNode cur) {
+            if (cur == null) {
+                return pre;
+            }
+            ListNode tmp = cur.next;
+            cur.next = pre;
+//            pre = cur;
+//            cur = tmp;
+            return reverse(cur, tmp);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

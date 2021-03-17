6b9179f25b83d9c4ba03299cb1fec4ddde12a8de
Huali Zhang <zhanghl@rd.netease.com>
2021-03-17 21:52:24 +0800

    给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）

---
+++ src/main/java/leetcode/Job/TreeNodeItem/_107BinaryTreeLevelOrderTraversalIi.java
@@ -0,0 +1,92 @@
+package leetcode.Job.TreeNodeItem;
+
+//给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历） 
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其自底向上的层序遍历为： 
+//
+// 
+//[
+//  [15,7],
+//  [9,20],
+//  [3]
+//]
+// 
+// Related Topics 树 广度优先搜索 
+// 👍 420 👎 0
+
+
+import java.util.*;
+
+public class _107BinaryTreeLevelOrderTraversalIi {
+    public static void main(String[] args) {
+        Solution solution = new _107BinaryTreeLevelOrderTraversalIi().new Solution();
+    }
+    /**
+    
+    */
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ArrayList<Integer> tempChen = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = queue.poll();
+                tempChen.add(temp.val);
+                if (temp.left != null) {
+                    queue.add(temp.left);
+                }
+                if (temp.right != null) {
+                    queue.add(temp.right);
+                }
+            }
+            result.add(tempChen);
+        }
+        Collections.reverse(result);
+        return result;
+    }
+}
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

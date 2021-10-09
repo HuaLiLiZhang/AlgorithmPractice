52a3eaa42351ea107e76cd4c307abc1776bf4ca0
Huali Zhang <zhanghl@rd.netease.com>
2021-10-09 10:30:00 +0800

    修改TreeNode

--- src/main/java/leetcode/Job/TreeNode.java
+++ src/main/java/leetcode/Job/TreeNode.java
@@ -5,8 +5,8 @@ package leetcode.Job;
  */
 public class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
+    public TreeNode left;
+    public TreeNode right;
 
     TreeNode(int val) {
         this.val = val;

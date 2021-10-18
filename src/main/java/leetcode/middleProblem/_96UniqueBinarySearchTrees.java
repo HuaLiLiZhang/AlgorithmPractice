28ec19514fc898901c55fbdf1612fa7c67a50967
Huali Zhang <zhanghl@rd.netease.com>
2021-10-18 11:19:46 +0800

    不同的二叉搜索树的数量

--- src/main/java/leetcode/middleProblem/_96UniqueBinarySearchTrees.java
+++ src/main/java/leetcode/middleProblem/_96UniqueBinarySearchTrees.java
@@ -60,6 +60,8 @@ public class _96UniqueBinarySearchTrees {
             dp[0] = 1;
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= i; j++) {
+                    //对于第i个节点，需要考虑1作为根节点直到i作为根节点的情况，所以需要累加
+                    //一共i个节点，对于根节点j时,左子树的节点个数为j-1，右子树的节点个数为i-j
                     dp[i] += dp[i - j] * dp[j - 1];
                 }
             }

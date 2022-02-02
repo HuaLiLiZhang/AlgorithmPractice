be80b6d9823ea0ce13f9c5127fe6cf0120de39de
zhanghuali <zhanghuali.zhl@alibaba-inc.com>
2022-02-02 14:23:38 +0800

    ADD：翻转单词前缀

---
+++ src/main/java/leetcode/dailyTopic/ReversePrefixOfWord.java
@@ -0,0 +1,42 @@
+package leetcode.dailyTopic;
+
+import com.sun.deploy.util.StringUtils;
+import jdk.internal.util.Preconditions;
+
+import java.util.Objects;
+
+/**
+ * TODO 类的实现描述
+ *
+ * @author zhanghuali
+ * @version 1.0
+ * @ClassName: ReversePrefixOfWord
+ * @CreateTime: 2022/02/02 14:07:30
+ * @Description:
+ */
+public class ReversePrefixOfWord {
+    public static void main(String[] args) {
+        String word = "abcdefd";
+        char ch = 'd';
+        System.out.println(new ReversePrefixOfWord().reversePrefix(word, ch));
+    }
+
+    public String reversePrefix(String word, char ch) {
+        if (word == null || word.length() <= 0) {
+            return word;
+        }
+        int index = word.indexOf(ch);
+        if (index <= -1) {
+            return word;
+        }
+        int start = index;
+        StringBuffer sb = new StringBuffer();
+        while (index >= 0) {
+            sb.append(word.charAt(index));
+            index--;
+        }
+        sb.append(word.substring(start + 1));
+        return sb.toString();
+    }
+
+}
